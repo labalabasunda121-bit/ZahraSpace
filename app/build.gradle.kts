427d53c6efc64225767ceb51ddcec4b7a8966e12
labalabasunda121-bit <labalabasunda121@gmail.com>
2026-03-19 22:40:05 +0700

    Remove Filament dependencies (3D tetap pakai shape sederhana)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,6 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.51")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.android.filament:filament-android:1.51.0")
-    implementation("com.google.android.filament:gltfio-android:1.51.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
